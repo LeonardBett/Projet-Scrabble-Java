32c4fa30ebda6e3d27907461dd33380432047549
Leonard Bettcher <leonard.bettcher@etu.u-bordeaux.fr>
2026-01-15 15:10:03 +0100

    premiere architecture + compilation confguré avec maven + cahier des charges SPECS.md ajouté

---
+++ src/main/java/fr/u_bordeaux/Main.java
@@ -0,0 +1,8 @@
+package fr.u_bordeaux;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        System.out.println("Lancement de l'application Scrabble...");
+    }
+}
