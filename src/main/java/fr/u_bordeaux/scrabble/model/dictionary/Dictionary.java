a5dc11345c47a2c8eae69d4f43a6618858b4d865
Yanis Blothiaux <yanis.blothiaux@etu.u-bordeaux.fr>
2026-02-05 16:40:03 +0100

    first try gaddag, not finish (changes has to be made)

--- src/main/java/fr/u_bordeaux/scrabble/model/dictionary/Dictionary.java
+++
@@ -1,8 +0,0 @@
-package fr.u_bordeaux.scrabble.model.dictionary;
-
-/**
- * Interface for dictionary data structures (DAWG, GADDAG).
- * Allows checking the validity of words.
- */
-public interface Dictionary {
-}
