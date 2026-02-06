3f68972304c62974b3aadb3cdd5c03e586e54ca6
Leonard Bettcher <leonard.bettcher@u-bordeaux.fr>
2026-02-06 13:21:42 +0100

    F38 finished => timeout implemented, google coding style and javadoc applied

--- src/main/java/fr/u_bordeaux/scrabble/model/network/ServerInfo.java
+++ src/main/java/fr/u_bordeaux/scrabble/model/network/ServerInfo.java
@@ -1,49 +1,79 @@
 package fr.u_bordeaux.scrabble.model.network;
 
+/** The type Server info. */
 // Store data of server, use for the Map of active server of DiscoveryService
 public class ServerInfo {
-    private String ip;
-    private int port;
-    private String name;
+  private final String ip;
+  private final int port;
+  private final String name;
 
-    // We will be changed for 30000 in final version
-    public static final int SERVER_TIMEOUT = 15000;
+  /** The constant SERVER_TIMEOUT. */
+  // We will be changed for 30000 in final version
+  public static final int SERVER_TIMEOUT = 15000;
 
-    // Store the last time this server was seen
-    private long lastSeen;
+  // Store the last time this server was seen
+  private long lastSeen;
 
-    public ServerInfo(String ip, int port, String name) {
-        this.ip = ip;
-        this.port = port;
-        this.name = name;
-        this.lastSeen = System.currentTimeMillis();
-    }
+  /**
+   * Instantiates a new Server info.
+   *
+   * @param ip the ip
+   * @param port the port
+   * @param name the name
+   */
+  public ServerInfo(String ip, int port, String name) {
+    this.ip = ip;
+    this.port = port;
+    this.name = name;
+    this.lastSeen = System.currentTimeMillis();
+  }
 
-    // Update the last time this server was seen, when we receive a signal
-    public void updateLastSeen(){
-        this.lastSeen = System.currentTimeMillis();
-    }
+  /** Update last seen. */
+  // Update the last time this server was seen, when we receive a signal
+  public void updateLastSeen() {
+    this.lastSeen = System.currentTimeMillis();
+  }
 
-    // Return true if this server is expired, false otherwise
-    public boolean isExpired(){
-        return (System.currentTimeMillis() - lastSeen) > SERVER_TIMEOUT;
-    }
+  /**
+   * Is expired boolean.
+   *
+   * @return the boolean
+   */
+  // Return true if this server is expired, false otherwise
+  public boolean isExpired() {
+    return (System.currentTimeMillis() - lastSeen) > SERVER_TIMEOUT;
+  }
 
-    // Getter
-    public String getIp() {
-        return ip;
-    }
+  /**
+   * Gets ip.
+   *
+   * @return the ip
+   */
+  // Getter
+  public String getIp() {
+    return ip;
+  }
 
-    public int getPort() {
-        return port;
-    }
+  /**
+   * Gets port.
+   *
+   * @return the port
+   */
+  public int getPort() {
+    return port;
+  }
 
-    public String getName() {
-        return name;
-    }
+  /**
+   * Gets name.
+   *
+   * @return the name
+   */
+  public String getName() {
+    return name;
+  }
 
-    @Override
-    public String toString(){
-        return "(name=" + name + ", ip=" + ip + ", port=" + port + ")";
-    }
+  @Override
+  public String toString() {
+    return "(name=" + name + ", ip=" + ip + ", port=" + port + ")";
+  }
 }
