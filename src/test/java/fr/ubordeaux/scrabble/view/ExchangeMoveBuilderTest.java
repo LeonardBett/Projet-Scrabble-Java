e34deece4acbb2759d35a9150c54705446172312
THOMAS CODOGNAN <thomas.codognan@etu.u-bordeaux.fr>
2026-04-08 14:51:18 +0200

    Remove unused GameView and ExchangeMoveBuilder classes; update tests to use controllers directly

--- src/test/java/fr/ubordeaux/scrabble/view/ExchangeMoveBuilderTest.java
+++ src/test/java/fr/ubordeaux/scrabble/view/ExchangeMoveBuilderTest.java
@@ -4,13 +4,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
+import fr.ubordeaux.scrabble.controller.builders.ExchangeMoveBuilderController;
 import fr.ubordeaux.scrabble.model.core.HumanPlayer;
 import fr.ubordeaux.scrabble.model.core.Move;
 import fr.ubordeaux.scrabble.model.core.Tile;
 import fr.ubordeaux.scrabble.model.enums.MoveType;
 import fr.ubordeaux.scrabble.model.enums.PlayerColor;
 import fr.ubordeaux.scrabble.model.interfaces.Player;
-import fr.ubordeaux.scrabble.view.gui.builders.ExchangeMoveBuilder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -29,40 +29,40 @@ class ExchangeMoveBuilderTest {
 
   @Test
   void buildShouldReturnMoveWhenLettersAreInRack() {
-    Move move = ExchangeMoveBuilder.build("AB", player);
+    Move move = ExchangeMoveBuilderController.build("AB", player);
     assertNotNull(move);
     assertEquals(MoveType.EXCHANGE, move.getType());
   }
 
   @Test
   void buildShouldReturnNullWhenLetterNotInRack() {
-    Move move = ExchangeMoveBuilder.build("Z", player);
+    Move move = ExchangeMoveBuilderController.build("Z", player);
     assertNull(move);
   }
 
   @Test
   void buildShouldReturnNullWhenEmptyString() {
-    Move move = ExchangeMoveBuilder.build("", player);
+    Move move = ExchangeMoveBuilderController.build("", player);
     assertNull(move);
   }
 
   @Test
   void buildShouldReturnNullWhenDuplicateLetterNotInRack() {
     // Only one 'A' in rack, asking for two should fail
-    Move move = ExchangeMoveBuilder.build("AA", player);
+    Move move = ExchangeMoveBuilderController.build("AA", player);
     assertNull(move);
   }
 
   @Test
   void buildShouldReturnMoveForSingleLetter() {
-    Move move = ExchangeMoveBuilder.build("C", player);
+    Move move = ExchangeMoveBuilderController.build("C", player);
     assertNotNull(move);
     assertEquals(1, move.getTiles().size());
   }
 
   @Test
   void buildShouldReturnMoveForAllRackLetters() {
-    Move move = ExchangeMoveBuilder.build("ABCD", player);
+    Move move = ExchangeMoveBuilderController.build("ABCD", player);
     assertNotNull(move);
     assertEquals(4, move.getTiles().size());
   }
